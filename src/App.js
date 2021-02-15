53aa1b3f226ae891a8b3124f62e01a9b8c1c8839
usakiman <choiyw2@gmail.com>
2021-02-16 00:14:17 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home, Auth } from 'pages';
+import HeaderContainer from 'containers/Base/HeaderContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import storage from 'lib/storage';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as userActions from 'redux/modules/user';
+
+class App extends Component {
+
+
+    initializeUserInfo = async () => {
+        const loggedInfo = storage.get('loggedInfo'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+        if(!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
+
+        const { UserActions } = this.props;
+        UserActions.setLoggedInfo(loggedInfo);
+        try {
+            await UserActions.checkStatus();
+        } catch (e) {
+            storage.remove('loggedInfo');
+            window.location.href = '/auth/login?expired';
+        }
+    }
+
+    componentDidMount() {
+        this.initializeUserInfo();
+    }
+
+    render() {
+        return (
+            <div>
+                <HeaderContainer/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/auth" component={Auth}/>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(
+    null,
+    (dispatch) => ({
+        UserActions: bindActionCreators(userActions, dispatch)
+    })
+)(App);
